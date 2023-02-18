466320244a4f1c43c19d967c5e629c56c570991a
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-02-18 16:58:48 -0600

    added function for problem #3

--- js/app.js
+++ js/app.js
@@ -40,7 +40,8 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
@@ -51,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
-}
+    let firstTotal = sum(a,b);
+    let finalTotal = sum(firstTotal[0],c);
+    let firstProduct = multiply(a,b);
+    let finalProduct = multiply(firstProduct[0],c);
+    return [finalTotal[0], finalProduct[0],`${a} and ${b} and ${c} sum to ${finalTotal[0]}.`,`The product of ${a} and ${b} and ${c} is ${finalProduct[0]}.`];
+};
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
